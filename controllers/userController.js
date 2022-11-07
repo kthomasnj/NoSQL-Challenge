9e639015fd6900cc4b6387a88c8ca7e76d7f716c
Keith S Thomas <kthomasnj@hotmail.com>
2022-11-06 22:56:51 -0500

    Added update user by ID functionality

--- controllers/userController.js
+++ controllers/userController.js
@@ -36,11 +36,11 @@ module.exports = {
     },
     deleteUser(req, res) {
         User.findOneAndRemove({ _id: req.params.userId })
-        .then(async (user) =>
-            !user
-              ? res.status(404).json({ message: 'No user with that ID' })
-              : res.json({user})
-          )
+            .then(async (user) =>
+                !user
+                    ? res.status(404).json({ message: 'No user with that ID' })
+                    : res.json({ user })
+            )
             .catch((err) => {
                 console.log(err);
                 res.status(500).json(err);
@@ -48,15 +48,30 @@ module.exports = {
     },
     getSingleUser(req, res) {
         User.findOne({ _id: req.params.userId })
-          .select('-__v')
-          .then(async (user) =>
-            !user
-              ? res.status(404).json({ message: 'No user with that ID' })
-              : res.json({user})
-          )
-          .catch((err) => {
-            console.log(err);
-            return res.status(500).json(err);
-          });
-      }
+            .select('-__v')
+            .then(async (user) =>
+                !user
+                    ? res.status(404).json({ message: 'No user with that ID' })
+                    : res.json({ user })
+            )
+            .catch((err) => {
+                console.log(err);
+                return res.status(500).json(err);
+            });
+    },
+    updateUser(req, res) {
+        User.findOneAndUpdate(
+            { _id: req.params.userId },
+            { $set: { username: req.body.username } },
+            { runValidators: true, new: true }
+        )
+            .then((user) =>
+                !user
+                    ? res
+                        .status(404)
+                        .json({ message: 'No user found with that ID!' })
+                    : res.json(user)
+            )
+            .catch((err) => res.status(500).json(err));
+    }
 };
--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -3,10 +3,11 @@ const {
   getUser,
   createUser,
   getSingleUser,
+  updateUser,
   deleteUser
 } = require('../../controllers/userController');
 
 router.route('/').get(getUser).post(createUser);
-router.route('/:userId').delete(deleteUser).get(getSingleUser);
+router.route('/:userId').delete(deleteUser).get(getSingleUser).put(updateUser);
 
 module.exports = router;
