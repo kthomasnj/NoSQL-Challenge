73611b9a414b0c8103000010870d1b45408a1672
Keith S Thomas <kthomasnj@hotmail.com>
2022-11-11 16:18:32 -0500

    Added functionality run build

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -14,16 +14,36 @@ module.exports = () => {
       install: './src/js/install.js'
     },
     output: {
-      filename: '[name].bundle.js',
+      filename: 'bundle.js',
       path: path.resolve(__dirname, 'dist'),
     },
     plugins: [
-      
+      new HtmlWebpackPlugin({
+        template: './index.html',
+        title: 'Webpack Plugin'
+      })
     ],
 
     module: {
       rules: [
-        
+        {
+          test: /\.css$/i,
+          use: ['style-loader', 'css-loader']
+        },
+        {
+          test: /\.(png|svg|jpg|jpeg|gif)$/i,
+          type: 'asset/resource',
+        }, 
+        {
+          test: /\.m?js$/,
+          exclude: /(node_modules|bower_components)/,
+          Use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env']
+            }
+          }
+        }
       ],
     },
   };
