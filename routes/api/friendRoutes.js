ff06f56bdc10039f03a5668bfaba640acdc28fa2
Keith S Thomas <kthomasnj@hotmail.com>
2022-11-11 10:14:36 -0500

    Starting over

--- routes/api/friendRoutes.js
+++
@@ -1,9 +0,0 @@
-const router = require('express').Router();
-const {
-  addFriend,
-  removeFriend
-} = require('../../controllers/friendController');
-
-router.route('/:userId/friend/:friendId').post(addFriend).delete(removeFriend);
-
-module.exports = router;
